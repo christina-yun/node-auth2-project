4c83e7c2fa63c1e6fe3bd6d824fe31cf95cd25da
Christina Yun <poncheeyun@yahoo.com>
2021-10-12 14:59:53 -0700

    Cleaned up all the comments

--- api/auth/tokenbuilder.js
+++ api/auth/tokenbuilder.js
@@ -1,25 +1,21 @@
-const jwt = require('jsonwebtoken');
-const { JWT_SECRET } = require('../secrets/index');
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../secrets/index");
 
 function tokenBuilder(user) {
-    const payload = {
-        subject: user.user_id,
-        username: user.username,
-        role_name: user.role_name
-    }
+  const payload = {
+    subject: user.user_id,
+    username: user.username,
+    role_name: user.role_name,
+  };
 
-    const options = {
-        expiresIn: '1d'
-    }
+  const options = {
+    expiresIn: "1d",
+  };
 
-    const token = jwt.sign(
-        payload,
-        JWT_SECRET,
-        options,
-    )
-    return token
+  const token = jwt.sign(payload, JWT_SECRET, options);
+  return token;
 }
 
 module.exports = {
-    tokenBuilder
+  tokenBuilder,
 };
