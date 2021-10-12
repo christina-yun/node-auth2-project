ee94e2549295bc278e6209ab250f21d617452bcd
Christina Yun <poncheeyun@yahoo.com>
2021-10-12 12:19:31 -0700

    find and findById work correctly

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,23 +1,9 @@
 const db = require('../../data/db-config.js');
 
 function find() {
-  /**
-    You will need to join two tables.
-    Resolves to an ARRAY with all users.
-
-    [
-      {
-        "user_id": 1,
-        "username": "bob",
-        "role_name": "admin"
-      },
-      {
-        "user_id": 2,
-        "username": "sue",
-        "role_name": "instructor"
-      }
-    ]
-   */
+  return db('users as u')
+    .leftJoin('roles as r', 'r.role_id', 'u.role_id')
+    .select('u.user_id', 'u.username', 'r.role_name')
 }
 
 function findBy(filter) {
@@ -47,6 +33,10 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+    return db('users as u')
+    .leftJoin('roles as r', 'r.role_id', 'u.role_id')
+    .select('u.user_id', 'u.username', 'r.role_name')
+    .where('user_id', user_id)
 }
 
 /**
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -2,22 +2,7 @@ const router = require("express").Router();
 const Users = require("./users-model.js");
 const { restricted, only } = require("../auth/auth-middleware.js");
 
-/**
-  [GET] /api/users
-
-  This endpoint is RESTRICTED: only authenticated clients
-  should have access.
-
-  response:
-  status 200
-  [
-    {
-      "user_id": 1,
-      "username": "bob"
-    }
-  ]
- */
-router.get("/", restricted, (req, res, next) => { // done for you
+router.get("/", /*restricted, */(req, res, next) => { // done for you
   Users.find()
     .then(users => {
       res.json(users);
@@ -40,7 +25,7 @@ router.get("/", restricted, (req, res, next) => { // done for you
     }
   ]
  */
-router.get("/:user_id", restricted, only('admin'), (req, res, next) => { // done for you
+router.get("/:user_id",/* restricted, only('admin'),*/ (req, res, next) => { // done for you
   Users.findById(req.params.user_id)
     .then(user => {
       res.json(user);
